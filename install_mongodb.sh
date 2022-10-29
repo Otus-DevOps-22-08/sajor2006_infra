ffd01e0e7e62a93c42b57210b29f2015b3bdfaff
sajor <sajor2006@yandex.ru>
2022-10-29 07:00:34 +0000

    cloud-testapp

---
+++ install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+apt update && apt install -y mongodb-org
+
+systemctl enable --now mongodb.service
+systemctl status mongodb.service
