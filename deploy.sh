0ac44a78e309be700b00b3a993678169e44a8ac2
sajor <sajor2006@yandex.ru>
2022-10-29 07:36:23 +0000

    cloud-testapp

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-! rpm -q git-core && exit 1
 cd ~
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential &&  ruby -v && bundler -v
+sudo apt install -y ruby-full git ruby-bundler build-essential &&  ruby -v && bundler -v
